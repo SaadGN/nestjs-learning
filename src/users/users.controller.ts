243536b30610fd466916e9930e80f6bc7aabde7f
SaadGN <saadgn89@gmail.com>
2025-09-17 00:59:32 +0500

    DTO with route parameters

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, DefaultValuePipe, Get, Param, ParseIntPipe, Post, Query, ValidationPipe } from '@nestjs/common'
+import { Body, Controller, DefaultValuePipe, Get, Param, ParseBoolPipe, ParseIntPipe, Post, Query, ValidationPipe } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { createUserDto } from './dtos/create-users.dto'
+import { GetUserParamDto } from './dtos/get-user-param.dto'
 
 @Controller('users')
 export class UsersController {
@@ -18,23 +19,29 @@ export class UsersController {
     // // console.log(query.gender)
     // return userService.getAllUsers()
 
-    @Get()
-    getUsers(@Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
-        @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number) {
-        console.log(limit, page)
+    @Get(':isMarried')
+    getUsers(
+        @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+        @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+        @Param() param: GetUserParamDto) {
+        // console.log(limit, page)
+
+        console.log(param)
         return this.usersService.getAllUsers()
     }
+
+
     @Post()
     createUsers(@Body() user: createUserDto) {
         console.log(user instanceof createUserDto)
         return `new user is created`
     }
 
-    @Get(':id')
-    getUsersbyId(@Param('id', ParseIntPipe) id: any) {
-        console.log(typeof id, id)
-        return this.usersService.getUserbyId(id)
-    }
+    // @Get(':id')
+    // getUsersbyId(@Param('id', ParseIntPipe) id: any) {
+    //     console.log(typeof id, id)
+    //     return this.usersService.getUserbyId(id)
+    // }
     // getUsersbyId(@Param('id') param:any){               ******read id from parameter
     //     console.log(param)
     // }
