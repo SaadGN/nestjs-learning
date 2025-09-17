1bb6e6f7f9c1ecba5e758e52629557837accf711
SaadGN <saadgn89@gmail.com>
2025-09-17 23:00:45 +0500

    intra dependency created with tweet module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import { TweetController } from './tweet/tweet.controller';
+import { TweetModule } from './tweet/tweet.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController, UsersController],
+  imports: [UsersModule, TweetModule],
+  controllers: [AppController, UsersController, TweetController],
   providers: [AppService ,UsersService],
 })
 export class AppModule {}
