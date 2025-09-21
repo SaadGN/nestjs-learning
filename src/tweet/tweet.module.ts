13ff63621fc603cabd87a16400d34506a2432c9a
SaadGN <saadgn89@gmail.com>
2025-09-22 00:46:46 +0500

    1-many realation created [user -tweet]

--- src/tweet/tweet.module.ts
+++ src/tweet/tweet.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TweetService } from './tweet.service';
 import { TweetController } from './tweet.controller';
 import { UsersModule } from 'src/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tweet } from './tweet.entity';
 
 @Module({
   controllers:[TweetController],
   providers: [TweetService],
-  imports:[UsersModule]
+  imports:[UsersModule , TypeOrmModule.forFeature([Tweet])]
 })
 export class TweetModule {}
