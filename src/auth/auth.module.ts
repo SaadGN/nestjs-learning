85af5fe7232a90f9d1f15fa349136f76b39e61f6
saad <saadgn89@gmail.com>
2025-09-28 01:08:15 +0500

    created login api

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,17 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsersModule } from 'src/users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from '../users/users.module';
 import { BcryptProvider } from './provider/bcrypt.provider';
+import { HashingProvider } from './provider/hashing.provider';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, BcryptProvider],
-  imports:[UsersModule,
-    // ConfigModule.forFeature(authConfig)
-  ],
-  exports:[AuthService]
+  providers: [AuthService, {
+    provide:HashingProvider,
+    useClass:BcryptProvider
+  }],
+  imports:[forwardRef( () => UsersModule)],
+  exports:[AuthService,HashingProvider]
 })
 export class AuthModule {}
