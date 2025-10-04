a4c354a4ba5c89cee85bd45ec4bae3096a4c06be
saad <saadgn89@gmail.com>
2025-10-04 21:08:52 +0500

    corrected name

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,5 @@
 import { registerAs } from "@nestjs/config"
 
 export default registerAs('database', () => ({
-    environment : process.env.NODE_ENV || 'poduction',
+    environment : process.env.NODE_ENV || 'production',
 }))
